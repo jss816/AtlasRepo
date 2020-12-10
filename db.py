b9bf0ded0731cd35a09e9559826d500fe42ef0ce
jss816 <58234740+jss816@users.noreply.github.com>
2020-12-10 11:57:14 -0600

    Add files via upload

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ from datetime import date
 import re
 from discord import emoji
 from discord import Message
+import send_sms
 
 
 phDate = date.today()
@@ -25,14 +26,20 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Positions
 
 client = discord.Client()
 
-token = 'NzY2MDA4MjkyNjQ0MjI1MDc2.X4dG0A.mu4kHufR_N76veGdtvdg4ylVPa4'
+token = 'NzY2MDA4MjkyNjQ0MjI1MDc2.X4dG0A.1J9KRiqVfaky9o_GmUXF9SRGLCU'
 
 bot = commands.Bot(command_prefix='!')
+
 
 
 def sanitize(text):
     text.replace('@everyone', '@\u200beveryone') # \u200b is our condom
     return text
+
+@bot.command(name='sms')
+async def send_smss(ctx, message):
+    send_sms.send(message)
+    return await SendHomeMML('Sent!')
 
 
 @bot.command(name='say')
@@ -40,13 +47,14 @@ async def say(ctx, args):
     message = "@everyone"
     message = sanitize(message)
     await SendHomeMML(message)
-    await SendDevenMML(message)
+
     taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
     message = f"{args}" + "\n" + f"{taabluedisc}"
     MessageE = discord.Embed(title = "", description=message, colour=0x12341)
     Message = await SendHomeEML(MessageE)
     await Message.add_reaction(taabluedisc)
-    Message = await SendDevenEML(MessageE)
+    await SendMattEML(MessageE)
+    await SendOZEML(MessageE)
     await Message.add_reaction(taabluedisc)
 
     return
@@ -56,13 +64,12 @@ async def sayP(ctx, args, argl):
     message = "@everyone"
     message = sanitize(message)
     await SendHomeMML(message)
-    await SendDevenMML(message)
-    taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
-    message = f"{args}" + "\n" + f"{taabluedisc}"
+    message = f"{args}" + "\n"
     MessageE = discord.Embed(title="", description=message, colour = 0x12341)
     MessageE.set_image(url=argl)
     await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
+    await SendMattEML(MessageE)
+    await SendOZEML(MessageE)
     return
 
 @bot.command(name='open')
@@ -70,18 +77,17 @@ async def createSignal(ctx, symbol = "", strike = "", callp = "", expiry = "", p
     message = "@everyone"
     message = sanitize(message)
     await SendHomeMML(message)
-    await SendDevenMML(message)
-    kindred = discord.utils.get(bot.emojis, name='kindredd')
-    angrykindred = discord.utils.get(bot.emojis, name='angrykindredd')
-    taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
+    #await SendDevenMML(message)
+    green = discord.utils.get(bot.emojis, name='green')
+    red = discord.utils.get(bot.emojis, name='red')
     d = date.today()
-    strike = int(strike)
+    strike = float(strike)
     price = float(price)
     cur.execute("""INSERT INTO Positions (date, status, symbol, strike, callp, expiry, price, pandl) Values (?,?,?,?,?,?,?,?)""",
                 (d, 'Open', symbol, strike, callp, expiry, price, 0))
     con.commit()
 
-    Message = symbol + " " + str(strike) + " " + callp + " " + expiry + " @" + str(price) + '\n\n' + f"{taabluedisc}"
+    Message = symbol + " " + str(strike) + " " + callp + " " + expiry + " @" + str(price) + '\n\n'
     MessageE = discord.Embed(title = "**New Position**", description = Message, colour=1221312)
 
 
@@ -92,54 +98,47 @@ async def createSignal(ctx, symbol = "", strike = "", callp = "", expiry = "", p
     #await SendHomeMML(k)
 
     await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
+    await SendOZEML(MessageE)
+    await SendMattEML(MessageE)
     return
 
-@bot.command(name='showopen')
+@bot.command (name='showopen')
 async def showopen(ctx):
-    message = "@everyone"
-    message = sanitize(message)
-    await SendHomeMML(message)
-    await SendDevenMML(message)
-    kindred = discord.utils.get(bot.emojis, name='kindredd')
-    angrykindred = discord.utils.get(bot.emojis, name='angrykindredd')
-    taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
+    green = discord.utils.get(bot.emojis, name='green')
+    red = discord.utils.get(bot.emojis, name='red')
     cur.execute("Select * From positions where status = ?",('Open',))
     k = cur.fetchall()
     desC = []
     for row in k:
         if(row[7]>=0.00):
             s = ""
-            s += f"{kindred} " + str(row[1]) + " " + \
+            s += f"{green} " + str(row[1]) + " " + \
                  str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + \
                  " " + str(row[5]) + " " + str(row[6]) + " " + "|" + f" **P/L**: " + str(
-                row[7])
+                row[7]) + "%"
             desC.append(s)
 
         else:
             s = ""
-            s += f"{angrykindred} " + str(row[1]) + " " + \
+            s += f"{red} " + str(row[1]) + " " + \
                  str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + \
                  " " + str(row[5]) + " " + str(row[6]) + " " + "|" + f" **P/L**: " + str(
-                row[7])
+                row[7]) + "%"
             desC.append(s)
 
     desP = '\n'.join(desC)
-    message = '\n' + desP + '\n\n' + f"{taabluedisc}"
+    message = '\n' + desP + '\n\n'
     MessageE = discord.Embed(title="***Current Open Positions***" + "\n" + "================", description = message, colour = 0x3432A1)
     await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
+    await SendOZEML(MessageE)
+    await SendMattEML(MessageE)
     return
 
 
 @bot.command(name='todayPL')
 async def todayPL(ctx):
-    message = "@everyone"
-    message = sanitize(message)
-    await SendHomeMML(message)
-    await SendDevenMML(message)
-    kindred = discord.utils.get(bot.emojis, name='kindredd')
-    angrykindred = discord.utils.get(bot.emojis, name='angrykindredd')
+    green = discord.utils.get(bot.emojis, name='green')
+    red = discord.utils.get(bot.emojis, name='red')
     taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
     cur.execute("Select * FROM positions where date = ?",(phDate,))
     temp = cur.fetchall()
@@ -150,36 +149,33 @@ async def todayPL(ctx):
     for row in temp:
         if(row[7]>=0.00):
             s = ""
-            s += f"{kindred} " + str(row[1]) + " " + \
+            s += f"{green} " + str(row[1]) + " " + \
                  str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + \
                  " " + str(row[5]) + " " + str(row[6]) + " " + "|" + f" **P/L**: " + str(
-                row[7])
+                row[7]) + "%"
             desC.append(s)
 
         else:
             s = ""
-            s += f"{angrykindred} " + str(row[1]) + " " + \
+            s += f"{red} " + str(row[1]) + " " + \
                  str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + \
                  " " + str(row[5]) + " " + str(row[6]) + " " + "|" + f" **P/L**: " + str(
-                row[7])
+                row[7]) + "%"
             desC.append(s)
 
     desP = '\n'.join(desC)
 
-    MessageE = discord.Embed(title=f"**Todays P/L** {kindred}", description=desP, colour=0xfa9324)
+    MessageE = discord.Embed(title=f"**Todays P/L** {green}", description=desP, colour=0xfa9324)
     await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
+    await SendOZEML(MessageE)
+    await SendMattEML(MessageE)
     return
 
 @bot.command(name='showmonth')
 async def showmonth(ctx, args):
-    message = "@everyone"
-    message = sanitize(message)
-    await SendHomeMML(message)
-    await SendDevenMML(message)
-    kindred = discord.utils.get(bot.emojis, name='kindredd')
-    angrykindred = discord.utils.get(bot.emojis, name='angrykindredd')
-    taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
+    green = discord.utils.get(bot.emojis, name='green')
+    red = discord.utils.get(bot.emojis, name='red')
+
     d = date.month
     cur.execute('''Select * From Positions Where date > ?''',(args, ))
     await ctx.channel.send('Showing positions from')
@@ -188,49 +184,50 @@ async def showmonth(ctx, args):
     for row in k:
         if(row[7]>=0.00):
             s = ""
-            s += f"{kindred} " + str(row[1]) + " " + \
+            s += f"{green} " + str(row[1]) + " " + \
                  str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + \
                  " " + str(row[5]) + " " + str(row[6]) + " " + "|" + f" **P/L**: " + str(
-                row[7])
+                row[7]) + "%"
             desC.append(s)
 
         else:
             s = ""
-            s += f"{angrykindred} " + str(row[1]) + " " + \
+            s += f"{red} " + str(row[1]) + " " + \
                  str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + \
                  " " + str(row[5]) + " " + str(row[6]) + " " + "|" + f" **P/L**: " + str(
-                row[7])
+                row[7]) + "%"
             desC.append(s)
 
     desP = '\n'.join(desC)
 
-    MessageE = discord.Embed(title=f"**Monthly P/L** {kindred}", description=desP, colour=0xfa9324)
+    MessageE = discord.Embed(title=f"**Monthly P/L** {green}", description=desP, colour=0xfa9324)
     await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
+    await SendMattEML(MessageE)
+    await SendOZEML(MessageE)
+
     return
 
 @bot.command(name='close')
 async def close(ctx, args, strike, expiry):
-    taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
-
     cur.execute('''UPDATE Positions set status = ? where symbol = ? and strike = ? and expiry = ?''',('Closed', args, strike, expiry,))
     con.commit()
     message = "@everyone"
     message = sanitize(message)
     await SendHomeMML(message)
-    await SendDevenMML(message)
-    messages = args + " " + strike + " " + expiry + '\n\n' + f"{taabluedisc}"
+    await SendOZMML(message)
+    #await SendDevenMML(message)
+    messages = args + " " + strike + " " + expiry + '\n\n'
     MessageE = discord.Embed(title="***Closing position***", description=messages, colour = 0xAA4EB)
     await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
+    await SendOZEML(MessageE)
+    await SendMattEML(MessageE)
     await ctx.channel.send("Position Closed.")
     return
 
 @bot.command(name='update')
-async def update(ctx, symbol, strike, expiry, argv, argd):
-    kindred = discord.utils.get(bot.emojis, name='kindredd')
-    angrykindred = discord.utils.get(bot.emojis, name='angrykindredd')
-    taabluedisc = discord.utils.get(bot.emojis, name='taabluedisc')
+async def update(ctx, symbol, strike, expiry, argv):
+    green = discord.utils.get(bot.emojis, name='green')
+    red = discord.utils.get(bot.emojis, name='red')
     cur.execute("Select price FROM positions where symbol = ? AND strike = ? AND expiry = ?",(symbol, strike, expiry))
     temp = cur.fetchone()
     temp = temp[0]
@@ -240,24 +237,25 @@ async def update(ctx, symbol, strike, expiry, argv, argd):
 
     cur.execute("""UPDATE Positions set pandl = ? where symbol = ? and strike = ? AND expiry = ?""",(temp, symbol, strike, expiry))
     await ctx.channel.send("Position Updated.")
-
-    #argd = any bit of information wanting to be posted
-    messageToServers = argd + '\n' + "Updating " + symbol + " " + strike + " Position, Current Price is: " + argv + '\n\n' + f"{taabluedisc}"
-    MessageE = discord.Embed(title=f"**Update**{kindred} ", description=messageToServers, colour=8432223)
-    await SendHomeEML(MessageE)
-    await SendDevenEML(MessageE)
     con.commit()
     return
 
 
+
 async def SendHomeEML(embedded):
-    return await bot.get_channel(778498768223600643).send(embed=embedded)
+    return await bot.get_channel(755488059537096804).send(embed=embedded)
+
+async def SendOZEML(embedded):
+    return await bot.get_channel(699780761745752154).send(embed=embedded)
+
+async def SendOZMML(message):
+    return await bot.get_channel(699780761745752154).send(message)
 
 async def SendAtlasEML(embedded):
     return await bot.get_channel(736062290721374267).send(embed=embedded)
 
 async def SendHomeMML(message):
-    return await bot.get_channel(778498768223600643).send(message)
+    return await bot.get_channel(755488059537096804).send(message)
 
 async def SendAtlasMML(message):
     return await bot.get_channel(736062290721374267).send(message)
@@ -268,5 +266,7 @@ async def SendDevenEML(message):
 async def SendDevenMML(message):
     return await bot.get_channel(782841568557793310).send(message)
 
+async def SendMattEML(message):
+    return await bot.get_channel(783563415061921834).send(embed=message)
 
 bot.run(token)
